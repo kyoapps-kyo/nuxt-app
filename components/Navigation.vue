951c5660679765332a1dc1c16939df1574bfcec6
kyoapps-kyo <kyo@kyoapps.com>
2022-06-18 22:08:48 +0900

    调整navbar翻译内容

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,15 +1,15 @@
 <template>
   <ul class="menu bg-base-100" :class="{ 'menu-horizontal': horizontal }">
     <li>
-      <NuxtLink to="/Works">{{ $t("works") }} / Works</NuxtLink>
+      <NuxtLink to="/Works">{{ $t("works") }}</NuxtLink>
     </li>
     <li>
-      <NuxtLink to="/posts">{{ $t("posts") }} / Posts</NuxtLink>
+      <NuxtLink to="/posts">{{ $t("posts") }}</NuxtLink>
     </li>
     <li>
-      <NuxtLink to="https://github.com/kyoapps-kyo/nuxt-app" target="_blank"
-        >{{ $t("source") }} / Source</NuxtLink
-      >
+      <NuxtLink to="https://github.com/kyoapps-kyo/nuxt-app" target="_blank">{{
+        $t("source")
+      }}</NuxtLink>
     </li>
   </ul>
 </template>
